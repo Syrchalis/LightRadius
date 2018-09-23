3245c03f7f35d6108d755c89883069953e4f58f8
Syrchalis <41159904+Syrchalis@users.noreply.github.com>
2018-09-23 15:54:17 +0200

    Add files via upload

---
+++ Source/SYR_Additions/LightRadiusSettings.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace Syr_LightRadius
+{
+    public class LightRadiusSettings : ModSettings
+    {
+        public static bool innerLight = false;
+        public static bool outerLight = false;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look<bool>(ref innerLight, "SyrLightRadius_innerLight", true, true);
+            Scribe_Values.Look<bool>(ref outerLight, "SyrLightRadius_outerLight", false, true);
+        }
+    }
+}
